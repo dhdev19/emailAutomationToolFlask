f729572aca1208413e56ee7de0ebf47f2b440cd7
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2025-04-20 16:19:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@
 # Activate virtual environment (if using one)
 # source venv/bin/activate
 
-# Set environment variables from .env
-export $(grep -v '^#' .env | xargs)
+
 
 # Start Gunicorn with your app
 gunicorn --bind 0.0.0.0:8000 --workers 4 --threads 2 wsgi:application 
